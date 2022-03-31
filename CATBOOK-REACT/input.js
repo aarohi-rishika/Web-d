825840dd32b63caa051205d0be0464f5488ee59f
aarohi-rishika <101784904+aarohi-rishika@users.noreply.github.com>
2022-03-31 21:30:43 +0530

    Update input.js

--- CATBOOK-REACT/input.js
+++ CATBOOK-REACT/input.js
@@ -2,13 +2,13 @@ let inputDirection = {x:0, y:1};
 let lastInputDirection = {x:0, y:1};
 
 window.addEventListener('keydown', event => {
-    if (event.key === "ArrowUp" && lastInputDirection.y !== 0) {
+    if (event.key === "ArrowUp" && lastInputDirection.x !== 0) {
         inputDirection = {x:0, y: -1};
-    } else if (event.key === "ArrowDown" && lastInputDirection.y !== 0) {
+    } else if (event.key === "ArrowDown" && lastInputDirection.x !== 0) {
         inputDirection = {x:0, y: 1};
-    } else if (event.key === "ArrowLeft" && lastInputDirection.x !== 0) {
+    } else if (event.key === "ArrowLeft" && lastInputDirection.y !== 0) {
         inputDirection = {x:-1, y: 0};
-    } else if (event.key === "ArrowRight" && lastInputDirection.x !== 0) {
+    } else if (event.key === "ArrowRight" && lastInputDirection.y !== 0) {
         inputDirection = {x:1, y: 0};
     }
 });
