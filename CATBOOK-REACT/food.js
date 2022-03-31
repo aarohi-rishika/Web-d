70e3df23107b9f2d43dbfd4bcccf8f02c9fcb4d7
aarohi-rishika <101784904+aarohi-rishika@users.noreply.github.com>
2022-03-31 21:27:59 +0530

    Update food.js

--- CATBOOK-REACT/food.js
+++ CATBOOK-REACT/food.js
@@ -6,3 +6,12 @@ const updateFood = () => {
         food = getNewFoodPosition();
     }
 }
+
+// Don't change me!
+const drawFood = (gameBoard) => {
+    const foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food');
+    gameBoard.appendChild(foodElement);
+}
